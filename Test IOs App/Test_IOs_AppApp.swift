65aa5dfcc121d325bdfa633e82100d6098a813fa
Lenart Velkavrh <lenart.velkavrh@gmail.com>
2020-12-14 04:56:41 +0100

    Initial Commit

---
+++ Test IOs App/Test_IOs_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_IOs_AppApp.swift
+//  Test IOs App
+//
+//  Created by Lenart Velkavrh on 14/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Test_IOs_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
